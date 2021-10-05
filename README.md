1b9470c47f446f668eb9c5059bd28fd5d61861a6
Aleph Cadena <alephcadena@gmail.com>
2021-10-05 17:33:54 -0500

    properly finished the js

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # Password-Generator
 
-# Understanding
-Going through this assignment has definitely been the most difficult for me so far. I regrettably did not get the sight to work and I could not figure out how to achieve that. I experimented with quite a few methods, searched a lot through Google and I am aware that it was my lack of understanding for JavaScript that caused this.
+# Description
+Users upon loading the page, click the generate password button and are asked about a series of characteristics they would like their password to include or not include. Once all questions have been answered, a random password is generated for the user to use.
 
-*I will get this right, I just need more to experiment with JavaScript because so far, it has been more of a struggle to understand in comparison to HTML and CSS for me. But I do understand that as for this assignment I did not meet the requirements for completion.
-
-# Thought-Process
-The following is what I had in mind to do but could not figure out how to do it exactly.
- - List the variables that can be used in the generated password.
- - Add WindowPrompts asking the user how many characters, uppercase letters, and special characters they would like in their password.
- - Find a way to save that information to use in order to generate the password (this is where I struggled the most).
- - Create a function for generate password.
- - Add an event listener for the generate button.
-
-# Other-Ideas
-If I were for example, to add checkboxes to the HTML code with the variables there, instead of trying to do the window prompts, I think I would've been able to do better with that because I understand it more than I do Javascript at the moment. But I understand the benefits of JS, and I definitely do want to improve.
+# Live Link
+https://alephcadena.github.io/Password-Generator/
--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var generatePassword = document.querySelector("#generate");
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+="
-var length = prompt ("How many characters would you like?")
-var upper = prompt ("How many uppercase letters would you like?")
-var special = prompt ("How many special characters would you like?")
-
-
 
 // Write password to the #password input
-function generateBtn( ) {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-function generatePassword(length, characters) {
-  let password = "";
-  for (let i = 0; i < length; i++) {
-  password += characters.charAt(
-  Math.floor(Math.random() * characters.length)
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// My added code
+
+var special = ['!','@','#','$','%','^','&','*','_','-','?','(',')','+','.','/',':',';','<','>','=','[',']','^','~'];
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var nums = ['1','2','3','4','5','6','7','8','9','0'];
+
+
+function generatePassword() {
+  var possiblePassword = [];
+  var length = window.prompt('Length of password: ');
+
+  while (length < 8 || length > 128 || !Number.isInteger(Number(length))) {
+    length = prompt("Password length must be an integer between 8 and 128 characters. Please try again: ");
+  };
+
+  var checkspecial = window.confirm('Do you want to include special characters? ');
+  var checklower = window.confirm('Do you want to include lowercase letters? ');
+  var checkupper = window.confirm('Do you want to include uppercase letters? ');
+  var checknums = window.confirm('Do you want to include numbers? ');
+
+  if (checkspecial) {
+    possiblePassword = possiblePassword.concat(special);
+  };
+
+  if (checklower) {
+    possiblePassword = possiblePassword.concat(lower);
+  };
+
+  if (checkupper) {
+    possiblePassword = possiblePassword.concat(upper);
+  };
+
+  if (checknums) {
+    possiblePassword = possiblePassword.concat(nums);
+  };
+  
+  if (possiblePassword.length == 0) {
+    window.alert('Password must include some characters....');
+    return 'No Criteria selected. Try again';
+  } else {
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+      result = result + possiblePassword[Math.floor(Math.random() * possiblePassword.length)];
+    }
+    return result;
+  };
+};
