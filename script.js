9280b1ac626a403862ef9549391d190eaee93b81
Aleph Cadena <alephcadena@gmail.com>
2021-10-23 22:16:35 -0500

    updated

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#create");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = newPassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// My added code
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+var specialchar = ['!','@','#','$','%','^','&','*','_','-','?','(',')','+','.','/',':',';','<','>','=','[',']','^','~'];
 
-var special = ['!','@','#','$','%','^','&','*','_','-','?','(',')','+','.','/',':',';','<','>','=','[',']','^','~'];
-var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var nums = ['1','2','3','4','5','6','7','8','9','0'];
-
-
-function generatePassword() {
-  var possiblePassword = [];
+function newPassword() {
+  var np = [];
   var length = window.prompt('Length of password: ');
 
-  while (length < 8 || length > 128 || !Number.isInteger(Number(length))) {
-    length = prompt("Password length must be an integer between 8 and 128 characters. Please try again: ");
+  while (length < 8, length > 128, !Number.isInteger(Number(length))) {
+    length = prompt("Password must be at least 8 integers. Please try again: ");
   };
 
-  var checkspecial = window.confirm('Do you want to include special characters? ');
-  var checklower = window.confirm('Do you want to include lowercase letters? ');
-  var checkupper = window.confirm('Do you want to include uppercase letters? ');
-  var checknums = window.confirm('Do you want to include numbers? ');
+  var special = window.confirm('Include special characters? ');
+  var lower = window.confirm('Include lowercase letters? ');
+  var upper = window.confirm('Include uppercase letters? ');
+  var nums = window.confirm('Include numbers? ');
 
-  if (checkspecial) {
-    possiblePassword = possiblePassword.concat(special);
+  if (special) {
+    np = np.concat(specialchar);
   };
-
-  if (checklower) {
-    possiblePassword = possiblePassword.concat(lower);
+  if (lower) {
+    np = np.concat(lowercase);
   };
-
-  if (checkupper) {
-    possiblePassword = possiblePassword.concat(upper);
+  if (upper) {
+    np = np.concat(uppercase);
   };
-
-  if (checknums) {
-    possiblePassword = possiblePassword.concat(nums);
+  if (nums) {
+    np = np.concat(numbers);
   };
   
-  if (possiblePassword.length == 0) {
-    window.alert('Password must include some characters....');
-    return 'No Criteria selected. Try again';
+  if (np.length == 0) {
+    window.alert('Password requires characters....');
+    return 'Nothing selected. Please try again';
   } else {
     var result = '';
     for ( var i = 0; i < length; i++ ) {
-      result = result + possiblePassword[Math.floor(Math.random() * possiblePassword.length)];
+      result = result + np[Math.floor(Math.random() * np.length)];
     }
     return result;
   };
